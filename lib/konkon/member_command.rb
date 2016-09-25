2b00631e550958719a250b06e68abbfa9b7ca5fb
ta1kt0me <p.wadachi@gmail.com>
2016-09-25 17:56:13 +0900

    Remove not important option

--- lib/konkon/member_command.rb
+++ lib/konkon/member_command.rb
@@ -9,7 +9,6 @@ module Konkon
     # bar@example.com
     # ```
     desc 'import GROUP FILE', 'import user mailaddress(name <email>)'
-    option :output, type: :string, aliases: '-o', desc: 'Output file'
     def import(group, file)
       validate_arguments group, file
 
@@ -18,18 +17,12 @@ module Konkon
         members: File.open(file).each_with_object([]) { |line, result| result << line }
       )
       page.register
-      output(page.members, options[:output]) if options[:output]
     end
 
     private
 
     def validate_arguments(group, file)
       raise unless File.exist?(file) || !group.empty?
-    end
-
-    def output(members, file)
-      emails = members.map { |s| s.match(/<(?<email>.*@.*)>/)[:email] }
-      File.write("./tmp/#{file}", emails.join("\n"))
     end
   end
 end
