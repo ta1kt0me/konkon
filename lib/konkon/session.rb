ba64b365637feebbe9248f12bab5fae516998866
ta1kt0me <p.wadachi@gmail.com>
2016-09-25 17:23:08 +0900

    Change UA to env

--- lib/konkon/session.rb
+++ lib/konkon/session.rb
@@ -13,9 +13,8 @@ module Konkon
 
       session = Capybara::Session.new(:poltergeist)
       session.driver.headers = {
-          'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2564.97 Safari/537.36'
+        'User-Agent' => ENV['UA']
       }
-
       session.visit 'https://manage.doorkeeper.jp/'
       session.driver.set_cookie 'remember_user_token', ENV['REMEMBER_USER_TOKEN']
       session.driver.set_cookie 'usdksc', ENV['USDKSC']
