bdc377f49c79aac47ee4763c96ae55fe73c957bf
ta1kt0me <p.wadachi@gmail.com>
2016-09-25 16:04:46 +0900

    Delegate building session to module

--- lib/konkon/member.rb
+++ lib/konkon/member.rb
@@ -1,8 +1,11 @@
 require 'thor'
 require 'capybara/poltergeist'
+require 'konkon/session'
 
 module Konkon
   class Member < Thor
+    include Session
+
     desc 'import GROUP FILE', 'import user mailaddress(name <email>)'
     option :output, type: :string, aliases: '-o', desc: 'Output file'
     def import(group, file)
@@ -28,23 +31,7 @@ module Konkon
     end
 
     def visit_first(group)
-      Capybara.register_driver :poltergeist do |app|
-        Capybara::Poltergeist::Driver.new(
-          app,
-          js_errors: false,
-          timeout: 5000
-        )
-      end
-
-      session = Capybara::Session.new(:poltergeist)
-      session.driver.headers = {
-          'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2564.97 Safari/537.36'
-      }
-
-      session.visit 'https://manage.doorkeeper.jp/'
-      session.driver.set_cookie 'remember_user_token', ENV['REMEMBER_USER_TOKEN']
-      session.driver.set_cookie 'usdksc', ENV['USDKSC']
-
+      session = build_session
       url = "https://manage.doorkeeper.jp/groups/#{group}/members/new"
       session.visit(url)
       session.visit(url)
